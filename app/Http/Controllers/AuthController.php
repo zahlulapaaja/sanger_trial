4f8ee16b443bd592463d1f74a091eaf51cbb5bfa
Zahlul <zahlul.2001@gmail.com>
2025-04-26 18:19:52 +0700

    berhasil authentikasi

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    public function login_proses(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'email'    => 'required|email',
+            'password'    => 'required',
+        ]);
+
+        // Check if email exists in the database
+        if (!User::where('email', $request->email)->exists()) {
+            return back()->withErrors(['email' => 'Email does not exist.'])->withInput();
+        }
+
+        // gatau ini untuk apa
+        // if (Auth::attempt($credentials)) {
+        //     $request->session()->regenerate();
+
+        //     return redirect()->intended('landing');
+        // }
+
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        // Attempt login
+        if (Auth::attempt($data)) {
+            $user = Auth::user();
+            Session::put('user_id', $user->id);
+            Session::put('name', $user->name);
+            Session::put('role', User::find($user->id)->roles);
+            Session::put('email', User::find($user->id)->email);
+            Session::put('avatar', User::find($user->id)->image);
+            return redirect()->route('home');
+        } else {
+            // Authentication failed
+            return back()->withErrors(['password' => 'Invalid password.'])->withInput();
+        }
+    }
+
+    public function logout()
+    {
+        if (session()->has('name')) {
+            session()->pull('user_id');
+            session()->pull('name');
+            session()->pull('role');
+            session()->pull('email');
+            session()->pull('avatar');
+        }
+        Auth::logout();
+        return redirect()->route('login')->with('success', 'Anda berhasil logout');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::view('/login', 'auth.login')->name('login');
+Route::post('/login-proses', [AuthController::class, 'login_proses'])->name('login.proses');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 });
